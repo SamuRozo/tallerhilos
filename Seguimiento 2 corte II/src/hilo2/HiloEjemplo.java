15e2e6fbed79270518a7cf5dbd5080ca1a4cc956
SamuRozo <158489295+SamuRozo@users.noreply.github.com>
2024-10-29 22:11:56 -0500

    Add files via upload

---
+++ Seguimiento 2 corte II/src/hilo2/HiloEjemplo.java
@@ -0,0 +1,18 @@
+package hilo2;
+
+import java.time.Duration;
+
+public class HiloEjemplo extends Thread{
+
+    @Override
+    public  void  run() {
+        try {
+            System.out.println("Hola soy un hilo");
+            Thread.sleep(Duration.ofMillis(4000));
+            System.out.println("Adios soy un hilo");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
